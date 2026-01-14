cc0a442462eff7598217d95c2e9957c4c4e83863
Md Sakib Mia <sakib01181@gmail.com>
2026-01-14 16:30:45 +0600

    Navbar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar1 } from "@/components/navbar1";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar1 className="justify-center items-center flex mx-auto px-2"></Navbar1>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-     <Button  variant="outline">Click Me</Button>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Next.js!
+      </h1>
     </div>
   );
 }
